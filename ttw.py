6ae1620441d2b0bf718d6e6e9d2efa437a3d2fc1
Gutteridge Alex <alexey.18.2014@gmail.com>
2020-09-21 23:47:29 +0100

    New design

--- ttw.py
+++ ttw.py
@@ -8,6 +8,7 @@ from PyQt5.QtChart import QChart, QChartView, QValueAxis, QBarCategoryAxis, QBar
 from PyQt5.Qt import Qt
 from PyQt5.QtGui import QPainter
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
 from DBmanage import DBhandle
 from matplotlib import pyplot as plt
 import matplotlib.backends.backend_tkagg
@@ -79,59 +80,53 @@ mouse_thread.start()
 
 
 # EXTERNAL FUNCTIONS
-def DisplayData(DateRange):
-	data = DBhandle.DBextract(DateRange)
-	if len(data) < 2:
-		data["NotEnoughData"] = 0
-	
-	class window(QMainWindow):
+data = DBhandle.DBextract("Weekly")
+header = "Weekly"
+
+class window(QWidget):
 		def __init__(self, parent=None):
-			super(window, self).__init__(parent=parent)
-			self.resize(800, 600)
-
-			set0 = QBarSet(f'Hours')
-
-			set0.append([float(x) for x in data.values()])
-
-			series = QBarSeries()
-			series.append(set0)
-
-			chart = QChart()
-			chart.addSeries(series)
-			chart.setTitle(DateRange)
-			chart.setAnimationOptions(QChart.SeriesAnimations)
+			QWidget.__init__(self, parent)
+			self.set0 = QBarSet(f'Hours')
 			
+			self.set0.append([float(x) for x in data.values()])
+			
+			self.series = QBarSeries()
+			self.series.append(self.set0)
+			
+			self.chart = QChart()
+			self.chart.setTheme(QChart.ChartThemeHighContrast)
+			self.chart.addSeries(self.series)
+			self.chart.setTitle(header)
+			self.chart.setAnimationOptions(QChart.SeriesAnimations)
+
 			months = (data.keys())
 
-			axisX = QBarCategoryAxis()
-			axisX.append(months)
+			self.axisX = QBarCategoryAxis()
+			self.axisX.append(months)
 
-			axisY = QValueAxis()
-			axisY.setRange(0, float(list(data.values())[0]))
-			if (float(list(data.values())[0])) > 19:
-				axisY.setTickCount(15)
-			elif (float(list(data.values())[0])) > 10:
-				axisY.setTickCount(10)
-			else:
-				axisY.setTickCount(5)
-			#axisY.setTickInterval(1)
-			#axisY.applyNiceNumbers()
+			self.axisY = QValueAxis()
+			try: 
+				self.axisY.setRange(0, float(list(data.values())[0]))
+				if (float(list(data.values())[0])) > 19:
+				    self.axisY.setTickCount(15)
+				elif (float(list(data.values())[0])) > 10:
+				    self.axisY.setTickCount(10)
+				else:
+				    self.axisY.setTickCount(5)
+				#axisY.setTickInterval(1)
+				#axisY.applyNiceNumbers()
+			except:
+				self.chart.setTitle("No Data")
+			self.chart.addAxis(self.axisX, Qt.AlignBottom)
+			self.chart.addAxis(self.axisY, Qt.AlignLeft)
 
-			chart.addAxis(axisX, Qt.AlignBottom)
-			chart.addAxis(axisY, Qt.AlignLeft)
+			self.chart.legend().setVisible(True)
+			self.chart.legend().setAlignment(Qt.AlignBottom)
 
-			chart.legend().setVisible(True)
-			chart.legend().setAlignment(Qt.AlignBottom)
-
-			chartView = QChartView(chart)
-			self.setCentralWidget(chartView)
-
-	window = window()
-	window.show()
-
-	def remove(e):
-		window.deleteLater()
-	window.closeEvent = remove
+			self.chartView = QChartView(self.chart)
+			self.vert_l = QVBoxLayout()
+			self.vert_l.addWidget(self.chartView)
+			self.setLayout(self.vert_l)
 
 def CurrentState():
 	if Running == "Paused":
@@ -155,38 +150,23 @@ def Pause():
 	elif Running:
 		Running = "Paused"
 
-def Monthly():
-	DisplayData("Monthly")
-
-def Weekly():
-	DisplayData("Weekly")
-
-
-def Daily():
-	DisplayData("Daily")
-
-# SUPPORTED APPS
-def Sublime():
-	DisplayData("Sublime Text")
-
-def GoogleChrome():
-	DisplayData("Google Chrome")
-
 # QT GUI
 class Ui_MainWindow(object):
+
 	def __init__(self, MainWindow):
 		MainWindow.setObjectName("MainWindow")
 		MainWindow.setEnabled(True)
-		MainWindow.resize(625, 312)
+		MainWindow.resize(980, 628)
 		sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
 		sizePolicy.setHorizontalStretch(0)
 		sizePolicy.setVerticalStretch(0)
 		sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
 		MainWindow.setSizePolicy(sizePolicy)
-		MainWindow.setMinimumSize(QtCore.QSize(625, 312))
-		MainWindow.setMaximumSize(QtCore.QSize(625, 312))
+		MainWindow.setMinimumSize(QtCore.QSize(980, 628))
+		MainWindow.setMaximumSize(QtCore.QSize(980, 628))
 		MainWindow.setFocusPolicy(QtCore.Qt.ClickFocus)
 		MainWindow.setAutoFillBackground(False)
+		MainWindow.setStyleSheet("")
 		MainWindow.setAnimated(True)
 		MainWindow.setTabShape(QtWidgets.QTabWidget.Rounded)
 		self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -198,7 +178,7 @@ class Ui_MainWindow(object):
 		self.centralwidget.setSizePolicy(sizePolicy)
 		self.centralwidget.setObjectName("centralwidget")
 		self.DataOutputs = QtWidgets.QLabel(self.centralwidget)
-		self.DataOutputs.setGeometry(QtCore.QRect(20, 30, 151, 51))
+		self.DataOutputs.setGeometry(QtCore.QRect(20, 10, 151, 51))
 		font = QtGui.QFont()
 		font.setFamily("Century Gothic")
 		font.setPointSize(13)
@@ -212,70 +192,42 @@ class Ui_MainWindow(object):
 		self.DataOutputs.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
 		self.DataOutputs.setObjectName("DataOutputs")
 		self.Monthly = QtWidgets.QPushButton(self.centralwidget)
-		self.Monthly.clicked.connect(Monthly)
-		self.Monthly.setGeometry(QtCore.QRect(170, 30, 131, 51))
+		self.Monthly.setGeometry(QtCore.QRect(20, 60, 131, 51))
 		font = QtGui.QFont()
 		font.setFamily("Calibri Light")
 		font.setPointSize(13)
 		self.Monthly.setFont(font)
 		self.Monthly.setObjectName("Monthly")
 		self.Weekly = QtWidgets.QPushButton(self.centralwidget)
-		self.Weekly.clicked.connect(Weekly)
-		self.Weekly.setGeometry(QtCore.QRect(320, 30, 131, 51))
+		self.Weekly.setGeometry(QtCore.QRect(20, 120, 131, 51))
 		font = QtGui.QFont()
 		font.setFamily("Calibri Light")
 		font.setPointSize(13)
 		self.Weekly.setFont(font)
 		self.Weekly.setObjectName("Weekly")
 		self.Daily = QtWidgets.QPushButton(self.centralwidget)
-		self.Daily.clicked.connect(Daily)
-		self.Daily.setGeometry(QtCore.QRect(470, 30, 131, 51))
+		self.Daily.setGeometry(QtCore.QRect(20, 180, 131, 51))
 		font = QtGui.QFont()
 		font.setFamily("Calibri Light")
 		font.setPointSize(13)
 		self.Daily.setFont(font)
 		self.Daily.setObjectName("Daily")
-		self.SupportedApps = QtWidgets.QLabel(self.centralwidget)
-		self.SupportedApps.setGeometry(QtCore.QRect(20, 130, 151, 51))
-		font = QtGui.QFont()
-		font.setFamily("Century Gothic")
-		font.setPointSize(13)
-		font.setBold(False)
-		font.setItalic(True)
-		font.setWeight(50)
-		self.SupportedApps.setFont(font)
-		self.SupportedApps.setScaledContents(False)
-		self.SupportedApps.setWordWrap(False)
-		self.SupportedApps.setIndent(0)
-		self.SupportedApps.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
-		self.SupportedApps.setObjectName("SupportedApps")
 		self.Sublime = QtWidgets.QPushButton(self.centralwidget)
-		self.Sublime.clicked.connect(Sublime)
-		self.Sublime.setGeometry(QtCore.QRect(190, 130, 131, 51))
+		self.Sublime.setGeometry(QtCore.QRect(20, 270, 131, 51))
 		font = QtGui.QFont()
 		font.setFamily("Calibri Light")
 		font.setPointSize(13)
 		self.Sublime.setFont(font)
 		self.Sublime.setObjectName("Sublime")
 		self.Chrome = QtWidgets.QPushButton(self.centralwidget)
-		self.Chrome.clicked.connect(GoogleChrome)
-		self.Chrome.setGeometry(QtCore.QRect(340, 130, 131, 51))
+		self.Chrome.setGeometry(QtCore.QRect(20, 330, 131, 51))
 		font = QtGui.QFont()
 		font.setFamily("Calibri Light")
 		font.setPointSize(13)
 		self.Chrome.setFont(font)
 		self.Chrome.setObjectName("Chrome")
-		self.line_2 = QtWidgets.QFrame(self.centralwidget)
-		self.line_2.setGeometry(QtCore.QRect(0, 210, 641, 16))
-		font = QtGui.QFont()
-		font.setPointSize(20)
-		self.line_2.setFont(font)
-		self.line_2.setLineWidth(2)
-		self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
-		self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
-		self.line_2.setObjectName("line_2")
 		self.CurrentState = QtWidgets.QLabel(self.centralwidget)
-		self.CurrentState.setGeometry(QtCore.QRect(330, 240, 151, 51))
+		self.CurrentState.setGeometry(QtCore.QRect(20, 460, 151, 51))
 		font = QtGui.QFont()
 		font.setFamily("Century Gothic")
 		font.setPointSize(13)
@@ -289,15 +241,17 @@ class Ui_MainWindow(object):
 		self.CurrentState.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
 		self.CurrentState.setObjectName("CurrentState")
 		self.Pause = QtWidgets.QPushButton(self.centralwidget)
-		self.Pause.clicked.connect(Pause)
-		self.Pause.setGeometry(QtCore.QRect(90, 240, 131, 51))
+		self.Pause.setGeometry(QtCore.QRect(20, 550, 131, 51))
 		font = QtGui.QFont()
 		font.setFamily("Calibri Light")
 		font.setPointSize(13)
 		self.Pause.setFont(font)
+		self.Pause.setAutoDefault(False)
+		self.Pause.setDefault(False)
+		self.Pause.setFlat(False)
 		self.Pause.setObjectName("Pause")
 		self.State = QtWidgets.QLabel(self.centralwidget)
-		self.State.setGeometry(QtCore.QRect(480, 240, 151, 51))
+		self.State.setGeometry(QtCore.QRect(10, 490, 151, 51))
 		font = QtGui.QFont()
 		font.setFamily("Century Gothic")
 		font.setPointSize(18)
@@ -306,10 +260,33 @@ class Ui_MainWindow(object):
 		font.setWeight(50)
 		self.State.setFont(font)
 		self.State.setScaledContents(False)
+		self.State.setAlignment(QtCore.Qt.AlignCenter)
 		self.State.setWordWrap(False)
 		self.State.setIndent(0)
 		self.State.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
 		self.State.setObjectName("State")
+		self.widget = window(self.centralwidget)
+		self.widget.setUpdatesEnabled(True)
+		self.widget.setGeometry(QtCore.QRect(150, -30, 851, 691))
+		self.widget.setObjectName("widget")
+		self.widget.raise_()
+		self.DataOutputs.raise_()
+		self.CurrentState.raise_()
+		self.Sublime.raise_()
+		self.Daily.raise_()
+		self.Pause.raise_()
+		self.Monthly.raise_()
+		self.State.raise_()
+		self.Weekly.raise_()
+		self.Chrome.raise_()
+
+		self.Sublime.clicked.connect(lambda: self.update(DataRange = "Sublime Text"))
+		self.Chrome.clicked.connect(lambda: self.update(DataRange = "Google Chrome"))
+		self.Daily.clicked.connect(lambda: self.update(DataRange = "Daily"))
+		self.Weekly.clicked.connect(lambda: self.update(DataRange = "Weekly"))
+		self.Monthly.clicked.connect(lambda: self.update(DataRange = "Monthly"))
+		self.Pause.clicked.connect(Pause)
+
 		MainWindow.setCentralWidget(self.centralwidget)
 
 		self.retranslateUi(MainWindow)
@@ -322,12 +299,27 @@ class Ui_MainWindow(object):
 		self.Monthly.setText(_translate("MainWindow", "Monthly"))
 		self.Weekly.setText(_translate("MainWindow", "Weekly"))
 		self.Daily.setText(_translate("MainWindow", "Daily"))
-		self.SupportedApps.setText(_translate("MainWindow", "Supported Apps : "))
 		self.Sublime.setText(_translate("MainWindow", "Sublime Text"))
 		self.Chrome.setText(_translate("MainWindow", "Google Chrome"))
 		self.CurrentState.setText(_translate("MainWindow", "Current State : "))
 		self.Pause.setText(_translate("MainWindow", "Pause"))
 		self.State.setText(_translate("MainWindow", "State"))
+
+	def update(self, DataRange):
+		global data
+		global header
+		data = DBhandle.DBextract(DataRange)
+		header = DataRange
+		if len(data) < 2:
+			data["NotEnoughData"] = 0
+
+		self.widget.close()
+		del self.widget
+		self.widget = window(self.centralwidget)
+		self.widget.setUpdatesEnabled(True)
+		self.widget.setGeometry(QtCore.QRect(150, -30, 851, 691))
+		self.widget.setObjectName("widget")
+		self.widget.show()
 
 	def closeEvent(self, event):
 		if can_exit:
@@ -338,8 +330,6 @@ class Ui_MainWindow(object):
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow(MainWindow)
-state_thread = threading.Thread(target=CurrentState)
-state_thread.start()
 MainWindow.show()
 app.aboutToQuit.connect(Closing)
 timer = QtCore.QTimer()
